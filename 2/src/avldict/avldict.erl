1c82211b1fe2d68a077f321bfcc6c6908ee1626c
Roman Deev <roman.deev06@gmail.com>
2024-02-09 15:49:13 +0300

    formatter

--- 2/src/avldict/avldict.erl
+++ 2/src/avldict/avldict.erl
@@ -1,9 +1,7 @@
 -module(avldict).
--export([
-  empty_tree/0, insert/3, remove/2, find/2,
-  from_list/1, to_list/1,
-  merge/2, map_tree/2, filter_tree/2, foldl/3, foldr/3
-]).
+
+-export([empty_tree/0, insert/3, remove/2, find/2, from_list/1, to_list/1, merge/2,
+         map_tree/2, filter_tree/2, foldl/3, foldr/3]).
 
 -define(KEY(Node), element(1, Node)).
 -define(VALUE(Node), element(2, Node)).
@@ -17,149 +15,166 @@
 % S — Левый ребёнок узла (Smaller, ибо значение должно быть меньше, чем в текущем)
 % B — Правый ребёнок узла (Bigger, ибо значение должно быть больше, чем в текущем)
 
-empty_tree() -> ?NIL.
+empty_tree() ->
+    ?NIL.
 
-find(_Key, ?NIL) -> not_found;
-find(Key, {Key, Value, _, _, _}) -> Value;
-find(Key, {Key1, _, _, Smaller, _}) when Key < Key1 -> find(Key, Smaller);
-find(Key, {Key1, _, _, Bigger}) when Key > Key1 -> find(Key, Bigger).
+find(_Key, ?NIL) ->
+    not_found;
+find(Key, {Key, Value, _, _, _}) ->
+    Value;
+find(Key, {Key1, _, _, Smaller, _}) when Key < Key1 ->
+    find(Key, Smaller);
+find(Key, {Key1, _, _, Bigger}) when Key > Key1 ->
+    find(Key, Bigger).
 
-insert(Key, Value, ?NIL) -> {Key, Value, 1, ?NIL, ?NIL};
-
+insert(Key, Value, ?NIL) ->
+    {Key, Value, 1, ?NIL, ?NIL};
 insert(Key, Value, {K2, _V2, H2, S2, B2}) when Key == K2 ->
-  {Key, Value, H2, S2, B2};
-
+    {Key, Value, H2, S2, B2};
 insert(Key, Value, {K2, V2, _H2, S2, B2}) when Key < K2 ->
-  {K4, V4, _, S4, B4} = insert(Key, Value, S2),
-  balance(S4, K4, V4, B4, K2, V2, B2);
-
+    {K4, V4, _, S4, B4} = insert(Key, Value, S2),
+    balance(S4, K4, V4, B4, K2, V2, B2);
 insert(Key, Value, {K2, V2, _H2, S2, B2}) when Key > K2 ->
-  {K4, V4, _, S4, B4} = insert(Key, Value, B2),
-  balance(S2, K2, V2, S4, K4, V4, B4).
+    {K4, V4, _, S4, B4} = insert(Key, Value, B2),
+    balance(S2, K2, V2, S4, K4, V4, B4).
 
+balance({K1, V1, H1, S1, B1}, AK, AV, {K2, V2, H2, S2, B2}, BK, BV, {K3, V3, H3, S3, B3})
+    when H2 > H1, H2 > H3 ->
+    {K2,
+     V2,
+     H1 + 2,
+     {AK, AV, H1 + 1, {K1, V1, H1, S1, B1}, S2},
+     {BK, BV, H3 + 1, B2, {K3, V3, H3, S3, B3}}};
+balance({K1, V1, H1, S1, B1}, AK, AV, {K2, V2, H2, S2, B2}, BK, BV, {K3, V3, H3, S3, B3})
+    when H1 >= H2, H1 >= H3 ->
+    HB = max(H2, H3) + 1,
+    HA = max(H1, HB) + 1,
+    {AK,
+     AV,
+     HA,
+     {K1, V1, H1, S1, B1},
+     {BK, BV, HB, {K2, V2, H2, S2, B2}, {K3, V3, H3, S3, B3}}};
+balance({K1, V1, H1, S1, B1}, AK, AV, {K2, V2, H2, S2, B2}, BK, BV, {K3, V3, H3, S3, B3})
+    when H3 >= H1, H3 >= H2 ->
+    HA = max(H1, H2) + 1,
+    HB = max(HA, H3) + 1,
+    {BK,
+     BV,
+     HB,
+     {AK, AV, HA, {K1, V1, H1, S1, B1}, {K2, V2, H2, S2, B2}},
+     {K3, V3, H3, S3, B3}}.
 
-balance({K1, V1, H1, S1, B1}, AK, AV,
-    {K2, V2, H2, S2, B2}, BK, BV,
-    {K3, V3, H3, S3, B3}) when H2 > H1, H2 > H3 ->
-  {K2, V2, H1 + 2,
-    {AK, AV, H1 + 1, {K1, V1, H1, S1, B1}, S2},
-    {BK, BV, H3 + 1, B2, {K3, V3, H3, S3, B3}}
-  };
-
-balance({K1, V1, H1, S1, B1}, AK, AV,
-    {K2, V2, H2, S2, B2}, BK, BV,
-    {K3, V3, H3, S3, B3}) when H1 >= H2, H1 >= H3 ->
-  HB = max(H2, H3) + 1,
-  HA = max(H1, HB) + 1,
-  {AK, AV, HA,
-    {K1, V1, H1, S1, B1},
-    {BK, BV, HB, {K2, V2, H2, S2, B2}, {K3, V3, H3, S3, B3}}
-  };
-
-balance({K1, V1, H1, S1, B1}, AK, AV,
-    {K2, V2, H2, S2, B2}, BK, BV,
-    {K3, V3, H3, S3, B3}) when H3 >= H1, H3 >= H2 ->
-  HA = max(H1, H2) + 1,
-  HB = max(HA, H3) + 1,
-  {BK, BV, HB,
-    {AK, AV, HA, {K1, V1, H1, S1, B1}, {K2, V2, H2, S2, B2}},
-    {K3, V3, H3, S3, B3}
-  }.
-
-
-remove_min({Key, Value, _, ?NIL, B}) -> {{Key, Value, nil, nil, nil}, B, true};
-remove_min({Key, Value, _, S, ?NIL}) -> {S, {Key, Value, 1, ?NIL, ?NIL}, true};
+remove_min({Key, Value, _, ?NIL, B}) ->
+    {{Key, Value, nil, nil, nil}, B, true};
+remove_min({Key, Value, _, S, ?NIL}) ->
+    {S, {Key, Value, 1, ?NIL, ?NIL}, true};
 remove_min({Key, Value, _, Smaller, Bigger}) ->
-  {Min, {K, V, H, S, B}, IsLastCall} = remove_min(Smaller),
-  {KB, VB, HB, SB, BB} = Bigger,
-  case {IsLastCall, {K, V, H, S, B}, BB, HB} of
-    {true, ?NIL, ?NIL, 2} ->
-      {Min,
-        {?KEY(SB), ?VALUE(SB), 2,
-          {Key, Value, 1, ?NIL, ?NIL},
-          {KB, VB, 1, ?NIL, ?NIL}
-        }, false};
-    {true, ?NIL, _, _} ->
-      {Min,
-        {KB, VB, max(?HEIGHT(SB) + 1, ?HEIGHT(BB)) + 1,
-          {Key, Value, ?HEIGHT(SB) + 1,
-            ?NIL,
-            SB}, BB
-        }, false};
-    _ -> {Min, balance({K, V, H, S, B}, Key, Value, SB, KB, VB, BB), false}
-  end.
+    {Min, {K, V, H, S, B}, IsLastCall} = remove_min(Smaller),
+    {KB, VB, HB, SB, BB} = Bigger,
+    case {IsLastCall, {K, V, H, S, B}, BB, HB} of
+        {true, ?NIL, ?NIL, 2} ->
+            {Min,
+             {?KEY(SB), ?VALUE(SB), 2, {Key, Value, 1, ?NIL, ?NIL}, {KB, VB, 1, ?NIL, ?NIL}},
+             false};
+        {true, ?NIL, _, _} ->
+            {Min,
+             {KB,
+              VB,
+              max(?HEIGHT(SB) + 1, ?HEIGHT(BB)) + 1,
+              {Key, Value, ?HEIGHT(SB) + 1, ?NIL, SB},
+              BB},
+             false};
+        _ ->
+            {Min, balance({K, V, H, S, B}, Key, Value, SB, KB, VB, BB), false}
+    end.
 
-
-rm(_Key, ?NIL) -> not_found;
-rm(Key, {Key, _Value, _, ?NIL, ?NIL}) -> ?NIL;
-rm(Key, {Key, _Value, _, S, ?NIL}) -> S;
-rm(Key, {Key, _Value, _, ?NIL, B}) -> B;
+rm(_Key, ?NIL) ->
+    not_found;
+rm(Key, {Key, _Value, _, ?NIL, ?NIL}) ->
+    ?NIL;
+rm(Key, {Key, _Value, _, S, ?NIL}) ->
+    S;
+rm(Key, {Key, _Value, _, ?NIL, B}) ->
+    B;
 rm(Key, {Key, _Value, _, Smaller, Bigger}) ->
-  {Min, NewBigger, _} = remove_min(Bigger),
-  case Min of
-    ?NIL -> ?NIL;
-    {MinK, MinV, _, _, _} ->
-      {KS, VS, _, SS, BS} = Smaller,
-      balance(SS, KS, VS, BS, MinK, MinV, NewBigger)
-  end;
+    {Min, NewBigger, _} = remove_min(Bigger),
+    case Min of
+        ?NIL ->
+            ?NIL;
+        {MinK, MinV, _, _, _} ->
+            {KS, VS, _, SS, BS} = Smaller,
+            balance(SS, KS, VS, BS, MinK, MinV, NewBigger)
+    end;
 rm(Key, {Key1, Value1, _H1, Smaller, Bigger}) when Key < Key1 ->
-  R = rm(Key, Smaller),
-  case {R, Bigger} of
-    {not_found, _} -> not_found;
-    {?NIL, ?NIL} -> {Key1, Value1, 1, ?NIL, ?NIL};
-    {_, {K4, V4, _, S4, B4}} -> balance(R, Key1, Value1, S4, K4, V4, B4)
-  end;
+    R = rm(Key, Smaller),
+    case {R, Bigger} of
+        {not_found, _} ->
+            not_found;
+        {?NIL, ?NIL} ->
+            {Key1, Value1, 1, ?NIL, ?NIL};
+        {_, {K4, V4, _, S4, B4}} ->
+            balance(R, Key1, Value1, S4, K4, V4, B4)
+    end;
 rm(Key, {Key1, Value1, _H1, Smaller, Bigger}) when Key > Key1 ->
-  R = rm(Key, Bigger),
-  case {Smaller, R} of
-    {_, not_found} -> not_found;
-    {?NIL, ?NIL} -> {Key1, Value1, 1, ?NIL, ?NIL};
-    {{K4, V4, _, S4, B4}, _} -> balance(S4, K4, V4, B4, Key1, Value1, R)
-  end.
+    R = rm(Key, Bigger),
+    case {Smaller, R} of
+        {_, not_found} ->
+            not_found;
+        {?NIL, ?NIL} ->
+            {Key1, Value1, 1, ?NIL, ?NIL};
+        {{K4, V4, _, S4, B4}, _} ->
+            balance(S4, K4, V4, B4, Key1, Value1, R)
+    end.
 
 remove(K, T) ->
-  case rm(K, T) of
-    not_found -> T;
-    {_, NewT, _} -> NewT;
-    NewT -> NewT
-  end.
+    case rm(K, T) of
+        not_found ->
+            T;
+        {_, NewT, _} ->
+            NewT;
+        NewT ->
+            NewT
+    end.
 
+from_list(L) ->
+    lists:foldl(fun({K, V}, Acc) -> insert(K, V, Acc) end, ?NIL, L).
 
-from_list(L) -> lists:foldl(fun({K, V}, Acc) -> insert(K, V, Acc) end, ?NIL, L).
-
-
-to_list(?NIL) -> [];
+to_list(?NIL) ->
+    [];
 to_list({K, V, _, S, B}) ->
-  to_list(S) ++ [{K, V}] ++ to_list(B).
+    to_list(S) ++ [{K, V}] ++ to_list(B).
 
-
-map(?NIL, _) -> [];
+map(?NIL, _) ->
+    [];
 map({K, V, _, S, B}, F) ->
-  map(S, F) ++ [F({K, V})] ++ map(B, F).
+    map(S, F) ++ [F({K, V})] ++ map(B, F).
 
-map_tree(T, F) -> avldict:from_list(map(T, F)).
+map_tree(T, F) ->
+    avldict:from_list(map(T, F)).
 
-
-filter(?NIL, _) -> [];
+filter(?NIL, _) ->
+    [];
 filter({K, V, _, S, B}, F) ->
-  case F({K, V}) of
-    true -> filter(S, F) ++ [{K, V}] ++ filter(B, F);
-    false -> filter(S, F) ++ filter(B, F)
-  end.
+    case F({K, V}) of
+        true ->
+            filter(S, F) ++ [{K, V}] ++ filter(B, F);
+        false ->
+            filter(S, F) ++ filter(B, F)
+    end.
 
-filter_tree(T, F) -> avldict:from_list(filter(T, F)).
+filter_tree(T, F) ->
+    avldict:from_list(filter(T, F)).
 
-
-foldl(?NIL, _, Acc) -> Acc;
+foldl(?NIL, _, Acc) ->
+    Acc;
 foldl({K, V, _, S, B}, F, Acc) ->
-  Acc2 = F(foldl(S, F, Acc), {K, V}),
-  foldl(B, F, Acc2).
+    Acc2 = F(foldl(S, F, Acc), {K, V}),
+    foldl(B, F, Acc2).
 
-
-foldr(?NIL, _, Acc) -> Acc;
+foldr(?NIL, _, Acc) ->
+    Acc;
 foldr({K, V, _, S, B}, F, Acc) ->
-  foldr(S, F, F(foldr(B, F, Acc), {K, V})).
-
+    foldr(S, F, F(foldr(B, F, Acc), {K, V})).
 
 merge(X, Y) ->
-  foldl(Y, fun(AccNode, {K, V}) -> avldict:insert(K, V, AccNode) end, X).
+    foldl(Y, fun(AccNode, {K, V}) -> avldict:insert(K, V, AccNode) end, X).
--- 2/test/avldict/avldict_tests.erl
+++ 2/test/avldict/avldict_tests.erl
@@ -1,233 +1,250 @@
 -module(avldict_tests).
+
 -include_lib("eunit/include/eunit.hrl").
 
 -define(HEIGHT(Node), element(3, Node)).
 -define(NIL, {nil, nil, 0, nil, nil}).
 
-balance_check({nil, nil, H, nil, nil}) -> H == 0;
+balance_check({nil, nil, H, nil, nil}) ->
+    H == 0;
 balance_check({_, _, H, S, B}) ->
-  H1 = ?HEIGHT(S),
-  H2 = ?HEIGHT(B),
-  balance_check(S) and balance_check(B)
-    and (-2 < H2 - H1) and (H2 - H1 < 2)
+    H1 = ?HEIGHT(S),
+    H2 = ?HEIGHT(B),
+    balance_check(S)
+    and balance_check(B)
+    and (-2 < H2 - H1)
+    and (H2 - H1 < 2)
     and (H == max(H1, H2) + 1).
 
-
 insert_all(L) ->
-  %% Свойство 1. Дерево должно быть сбалансировано после выполнения любой операции
-  R = lists:foldl(fun({K, V}, Acc) ->
-    Cur = avldict:insert(K, V, Acc),
-    ?assertEqual(true, balance_check(Cur)),
-    Cur
-                  end,
-    avldict:empty_tree(),
-    L),
-  %% Свойство 2. Дерево должно быть бинарным. Т.е. в результате в ЛКП-обхода ключи не убывают
-  ?assertEqual(avldict:to_list(R), lists:usort(L)).
+    %% Свойство 1. Дерево должно быть сбалансировано после выполнения любой операции
+    R = lists:foldl(fun({K, V}, Acc) ->
+                       Cur = avldict:insert(K, V, Acc),
+                       ?assertEqual(true, balance_check(Cur)),
+                       Cur
+                    end,
+                    avldict:empty_tree(),
+                    L),
+    %% Свойство 2. Дерево должно быть бинарным. Т.е. в результате в ЛКП-обхода ключи не убывают
+    ?assertEqual(avldict:to_list(R), lists:usort(L)).
 
 empty_tree_test() ->
-  ?assertEqual(avldict:empty_tree(), ?NIL).
+    ?assertEqual(avldict:empty_tree(), ?NIL).
 
 one_node_test() ->
-  T = avldict:insert(1, 2, avldict:empty_tree()),
-  Target = {1, 2, 1, ?NIL, ?NIL},
-  ?assertEqual(T, Target),
-  ?assertEqual(avldict:find(0, T), not_found),
-  ?assertEqual(avldict:find(1, T), 2).
+    T = avldict:insert(1, 2, avldict:empty_tree()),
+    Target = {1, 2, 1, ?NIL, ?NIL},
+    ?assertEqual(T, Target),
+    ?assertEqual(avldict:find(0, T), not_found),
+    ?assertEqual(avldict:find(1, T), 2).
 
 two_node_test() ->
-  T1 = avldict:insert(1, 1, avldict:empty_tree()),
-  T2 = avldict:insert(2, 2, T1),
-  Target = {1, 1, 2,
-    ?NIL,
-    {2, 2, 1, ?NIL, ?NIL}},
-  ?assertEqual(T2, Target).
+    T1 = avldict:insert(1, 1, avldict:empty_tree()),
+    T2 = avldict:insert(2, 2, T1),
+    Target = {1, 1, 2, ?NIL, {2, 2, 1, ?NIL, ?NIL}},
+    ?assertEqual(T2, Target).
 
 simple_tree_test() ->
-  L = [{1, 2}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}],
-  Target = {6, 7, 3,
-    {2, 3, 2,
-      {1, 2, 1, ?NIL, ?NIL},
-      {4, 5, 1, ?NIL, ?NIL}},
-    {8, 9, 2,
-      ?NIL,
-      {10, 11, 1, ?NIL, ?NIL}}},
-  ?assertEqual(avldict:from_list(L), Target).
+    L = [{1, 2}, {2, 3}, {4, 5}, {6, 7}, {8, 9}, {10, 11}],
+    Target =
+        {6,
+         7,
+         3,
+         {2, 3, 2, {1, 2, 1, ?NIL, ?NIL}, {4, 5, 1, ?NIL, ?NIL}},
+         {8, 9, 2, ?NIL, {10, 11, 1, ?NIL, ?NIL}}},
+    ?assertEqual(avldict:from_list(L), Target).
 
 big_insert_test() ->
-  L = [{random:uniform(1_000_000), 0} || _ <- lists:seq(1, 1_000_000)],
-  T = avldict:from_list(L),
-  balance_check(T),
-  ?assertEqual(avldict:to_list(T), lists:usort(L)).
+    L = [{random:uniform(1_000_000), 0} || _ <- lists:seq(1, 1_000_000)],
+    T = avldict:from_list(L),
+    balance_check(T),
+    ?assertEqual(avldict:to_list(T), lists:usort(L)).
 
 %% Тесты операции удаления из дерева
 
 one_node_remove_test() ->
-  T = avldict:from_list([{1, 1}]),
-  ?assertEqual(avldict:remove(1, T), avldict:empty_tree()).
+    T = avldict:from_list([{1, 1}]),
+    ?assertEqual(avldict:remove(1, T), avldict:empty_tree()).
 
 simple_node_remove_test() ->
-  T = avldict:from_list([{1, 1}, {0, 0}]),
-  ?assertEqual(avldict:to_list(avldict:remove(1, T)), [{0, 0}]).
+    T = avldict:from_list([{1, 1}, {0, 0}]),
+    ?assertEqual(avldict:to_list(
+                     avldict:remove(1, T)),
+                 [{0, 0}]).
 
 simple_node2_remove_test() ->
-  T = avldict:from_list([{1, 1}, {0, 0}, {2, 2}]),
-  ?assertEqual(avldict:to_list(avldict:remove(2, T)), [{0, 0}, {1, 1}]),
-  ?assertEqual(avldict:to_list(avldict:remove(1, T)), [{0, 0}, {2, 2}]).
+    T = avldict:from_list([{1, 1}, {0, 0}, {2, 2}]),
+    ?assertEqual(avldict:to_list(
+                     avldict:remove(2, T)),
+                 [{0, 0}, {1, 1}]),
+    ?assertEqual(avldict:to_list(
+                     avldict:remove(1, T)),
+                 [{0, 0}, {2, 2}]).
 
 simple_node4_remove_test() ->
-  T = avldict:from_list([{1, 1}, {0, 0}, {2, 2}, {4, 4}]),
-  ?assertEqual(avldict:to_list(avldict:remove(2, T)), [{0, 0}, {1, 1}, {4, 4}]).
+    T = avldict:from_list([{1, 1}, {0, 0}, {2, 2}, {4, 4}]),
+    ?assertEqual(avldict:to_list(
+                     avldict:remove(2, T)),
+                 [{0, 0}, {1, 1}, {4, 4}]).
 
 simple_not_found_remove_test() ->
-  L = [{I, I} || I <- lists:seq(0, 3)],
-  T = avldict:from_list(L),
-  ?assertEqual(avldict:to_list(avldict:remove(42, T)), L).
+    L = [{I, I} || I <- lists:seq(0, 3)],
+    T = avldict:from_list(L),
+    ?assertEqual(avldict:to_list(
+                     avldict:remove(42, T)),
+                 L).
 
 remove_all(L, T) ->
-  {R, _} = lists:foldl(fun({K, _V}, {TAcc, LAcc}) ->
-    CurT = avldict:remove(K, TAcc),
-    [_ | CurL] = LAcc,
-    ?assertEqual(true, balance_check(CurT)),
-    ?assertEqual(lists:usort(CurL), avldict:to_list(CurT)),
-    {CurT, CurL}
-                       end,
-    {T, L},
-    L),
-  ?assertEqual(R, avldict:empty_tree()).
+    {R, _} =
+        lists:foldl(fun({K, _V}, {TAcc, LAcc}) ->
+                       CurT = avldict:remove(K, TAcc),
+                       [_ | CurL] = LAcc,
+                       ?assertEqual(true, balance_check(CurT)),
+                       ?assertEqual(lists:usort(CurL), avldict:to_list(CurT)),
+                       {CurT, CurL}
+                    end,
+                    {T, L},
+                    L),
+    ?assertEqual(R, avldict:empty_tree()).
 
 remove_all_test() ->
-  L = [{1, 1}, {2, 2}, {0, 0}, {4, 4}],
-  T = avldict:from_list(L),
-  remove_all(L, T).
+    L = [{1, 1}, {2, 2}, {0, 0}, {4, 4}],
+    T = avldict:from_list(L),
+    remove_all(L, T).
 
 remove_test_case(Size) ->
-  Set = sets:from_list([{random:uniform(Size), 0} || _ <- lists:seq(1, Size)]),
-  L = sets:to_list(Set),
-  T = avldict:from_list(L),
-  remove_all(L, T).
+    Set = sets:from_list([{random:uniform(Size), 0} || _ <- lists:seq(1, Size)]),
+    L = sets:to_list(Set),
+    T = avldict:from_list(L),
+    remove_all(L, T).
 
 small_remove_test() ->
-  [remove_test_case(random:uniform(6)) || _ <- lists:seq(1, 10000)].
+    [remove_test_case(random:uniform(6)) || _ <- lists:seq(1, 10000)].
 
 medium_remove_test() ->
-  [remove_test_case(random:uniform(20)) || _ <- lists:seq(1, 10000)].
+    [remove_test_case(random:uniform(20)) || _ <- lists:seq(1, 10000)].
 
 big_remove_test() ->
-  [remove_test_case(random:uniform(100)) || _ <- lists:seq(1, 10000)].
+    [remove_test_case(random:uniform(100)) || _ <- lists:seq(1, 10000)].
 
 %% Перебор возможных вставок элементов из n-элементов.
 %% Хорошее покрытие, но не проверяет поведение при вставке повторяющихся элементов
-perms([]) -> [[]];
-perms(L) -> [[H | T] || H <- L, T <- perms(L--[H])].
+perms([]) ->
+    [[]];
+perms(L) ->
+    [[H | T] || H <- L, T <- perms(L -- [H])].
 
 perms_n(X) ->
-  lists:map(fun(L) -> insert_all(L) end, perms([{I, 0} || I <- lists:seq(1, X)])).
+    lists:map(fun(L) -> insert_all(L) end, perms([{I, 0} || I <- lists:seq(1, X)])).
 
 all_inserts_perms_test_() ->
-  [
-    ?_test(perms_n(1)),
-    ?_test(perms_n(2)),
-    ?_test(perms_n(3)),
-    ?_test(perms_n(4)),
-    ?_test(perms_n(5)),
-    ?_test(perms_n(6)),
-    ?_test(perms_n(7)),
-    ?_test(perms_n(8)),
-    ?_test(perms_n(9))
-  ].
+    [?_test(perms_n(1)),
+     ?_test(perms_n(2)),
+     ?_test(perms_n(3)),
+     ?_test(perms_n(4)),
+     ?_test(perms_n(5)),
+     ?_test(perms_n(6)),
+     ?_test(perms_n(7)),
+     ?_test(perms_n(8)),
+     ?_test(perms_n(9))].
 
 %% Чтобы проверить работу с дубликатами проверим все размещения с возвратом
 %% Их n^n поэтому проверяет только для 7 элементов в словаре
 %% Зато это полное покрытие для всевозможных вставок в дерево
 placement_with_repetitions4_test() ->
-  L = lists:seq(1, 4),
-  A = [[{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}] || X <- L, Y <- L, Z <- L, Q <- L],
-  lists:map(fun insert_all/1, A).
+    L = lists:seq(1, 4),
+    A = [[{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}] || X <- L, Y <- L, Z <- L, Q <- L],
+    lists:map(fun insert_all/1, A).
 
 placement_with_repetitions5_test() ->
-  L = lists:seq(1, 5),
-  A = [
-    [{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}, {W, 0}]
-    || X <- L, Y <- L, Z <- L, Q <- L, W <- L],
-  lists:map(fun insert_all/1, A).
+    L = lists:seq(1, 5),
+    A = [[{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}, {W, 0}] || X <- L, Y <- L, Z <- L, Q <- L, W <- L],
+    lists:map(fun insert_all/1, A).
 
 placement_with_repetitions6_test() ->
-  L = lists:seq(1, 6),
-  A = [
-    [{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}, {W, 0}, {E, 0}]
-    || X <- L, Y <- L, Z <- L, Q <- L, W <- L, E <- L],
-  lists:map(fun insert_all/1, A).
+    L = lists:seq(1, 6),
+    A = [[{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}, {W, 0}, {E, 0}]
+         || X <- L, Y <- L, Z <- L, Q <- L, W <- L, E <- L],
+    lists:map(fun insert_all/1, A).
 
 placement_with_repetitions7_test() ->
-  L = lists:seq(1, 7),
-  A = [
-    [{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}, {W, 0}, {E, 0}, {R, 0}]
-    || X <- L, Y <- L, Z <- L, Q <- L, W <- L, E <- L, R <- L],
-  lists:map(fun insert_all/1, A).
+    L = lists:seq(1, 7),
+    A = [[{X, 0}, {Y, 0}, {Z, 0}, {Q, 0}, {W, 0}, {E, 0}, {R, 0}]
+         || X <- L, Y <- L, Z <- L, Q <- L, W <- L, E <- L, R <- L],
+    lists:map(fun insert_all/1, A).
 
 %% Тесты дополнительных функций, которые должен поддерживать словарь
 map_tree_test() ->
-  L = [{I, 0} || I <- lists:seq(1, 20)],
-  T = avldict:from_list(L),
-  T2 = avldict:map_tree(T, fun({K, V}) -> {K * 2, V} end),
-  L2 = lists:map(fun({K, V}) -> {K * 2, V} end, L),
-  ?assertEqual(L2, avldict:to_list(T2)).
+    L = [{I, 0} || I <- lists:seq(1, 20)],
+    T = avldict:from_list(L),
+    T2 = avldict:map_tree(T, fun({K, V}) -> {K * 2, V} end),
+    L2 = lists:map(fun({K, V}) -> {K * 2, V} end, L),
+    ?assertEqual(L2, avldict:to_list(T2)).
 
 filter_tree_test() ->
-  L = [{I, 0} || I <- lists:seq(1, 20)],
-  T = avldict:from_list(L),
-  T2 = avldict:filter_tree(T, fun({K, _V}) -> K rem 2 =:= 0 end),
-  L2 = lists:filter(fun({K, _V}) -> K rem 2 =:= 0 end, L),
-  ?assertEqual(L2, avldict:to_list(T2)).
+    L = [{I, 0} || I <- lists:seq(1, 20)],
+    T = avldict:from_list(L),
+    T2 = avldict:filter_tree(T, fun({K, _V}) -> K rem 2 =:= 0 end),
+    L2 = lists:filter(fun({K, _V}) -> K rem 2 =:= 0 end, L),
+    ?assertEqual(L2, avldict:to_list(T2)).
 
 %% Для свёрток выбрана конкатенация, ибо она не коммутативна
 %% Следовательно будет проверен порядок элементов
 foldl_tree_test() ->
-  L = [{I, 0} || I <- lists:seq(1, 20)],
-  T = avldict:from_list(L),
-  R = avldict:foldl(T, fun(Acc, {K, _V}) -> Acc ++ "|" ++ integer_to_list(K) end, ""),
-  R2 = lists:foldl(fun({K, _V}, Acc) -> Acc ++ "|" ++ integer_to_list(K) end, "", L),
-  ?assertEqual(R, R2).
+    L = [{I, 0} || I <- lists:seq(1, 20)],
+    T = avldict:from_list(L),
+    R = avldict:foldl(T, fun(Acc, {K, _V}) -> Acc ++ "|" ++ integer_to_list(K) end, ""),
+    R2 = lists:foldl(fun({K, _V}, Acc) -> Acc ++ "|" ++ integer_to_list(K) end, "", L),
+    ?assertEqual(R, R2).
 
 foldr_tree_test() ->
-  L = [{I, 0} || I <- lists:seq(1, 20)],
-  T = avldict:from_list(L),
-  R = avldict:foldr(T, fun(Acc, {K, _V}) -> Acc ++ "|" ++ integer_to_list(K) end, ""),
-  R2 = lists:foldr(fun({K, _V}, Acc) -> Acc ++ "|" ++ integer_to_list(K) end, "", L),
-  ?assertEqual(R, R2).
-
+    L = [{I, 0} || I <- lists:seq(1, 20)],
+    T = avldict:from_list(L),
+    R = avldict:foldr(T, fun(Acc, {K, _V}) -> Acc ++ "|" ++ integer_to_list(K) end, ""),
+    R2 = lists:foldr(fun({K, _V}, Acc) -> Acc ++ "|" ++ integer_to_list(K) end, "", L),
+    ?assertEqual(R, R2).
 
 %% Свойство 3. AVL-Dict должен быть моноидом
 %% А значит, должен быть единичный элемент...
 
 monoid_neutral_elem_test() ->
-  T = avldict:from_list([{1, 1}, {2, 2}, {3, 3}, {4, 4}]),
-  R = avldict:merge(T, avldict:empty_tree()),
-  ?assertEqual(T, R),
-  R2 = avldict:merge(avldict:empty_tree(), T),
-  ?assertEqual(T, R2).
+    T = avldict:from_list([{1, 1}, {2, 2}, {3, 3}, {4, 4}]),
+    R = avldict:merge(T, avldict:empty_tree()),
+    ?assertEqual(T, R),
+    R2 = avldict:merge(
+             avldict:empty_tree(), T),
+    ?assertEqual(T, R2).
 
 %% ... и должна существовать ассоциативная операция умножения,
 %% которой является слияния деревьев
 
 monoid_assoc_test_case(ASize, BSize, CSize) ->
-  A = avldict:from_list([{random:uniform(30), random:uniform(100)} || _ <- lists:seq(1, ASize)]),
-  B = avldict:from_list([{random:uniform(30), random:uniform(100)} || _ <- lists:seq(1, BSize)]),
-  C = avldict:from_list([{random:uniform(30), random:uniform(100)} || _ <- lists:seq(1, CSize)]),
+    A = avldict:from_list([{random:uniform(30), random:uniform(100)}
+                           || _ <- lists:seq(1, ASize)]),
+    B = avldict:from_list([{random:uniform(30), random:uniform(100)}
+                           || _ <- lists:seq(1, BSize)]),
+    C = avldict:from_list([{random:uniform(30), random:uniform(100)}
+                           || _ <- lists:seq(1, CSize)]),
 
-  R1 = avldict:merge(avldict:merge(A, B), C),
-  R2 = avldict:merge(A, avldict:merge(B, C)),
-  ?assertEqual(avldict:to_list(R1), avldict:to_list(R2)).
+    R1 = avldict:merge(
+             avldict:merge(A, B), C),
+    R2 = avldict:merge(A, avldict:merge(B, C)),
+    ?assertEqual(avldict:to_list(R1), avldict:to_list(R2)).
 
 monoid_assoc_small_test() ->
-  [monoid_assoc_test_case(random:uniform(10) - 1, random:uniform(10) - 1, random:uniform(10) - 1)
-    || _ <- lists:seq(1, 10000)].
+    [monoid_assoc_test_case(random:uniform(10) - 1,
+                            random:uniform(10) - 1,
+                            random:uniform(10) - 1)
+     || _ <- lists:seq(1, 10000)].
 
 monoid_assoc_medium_test() ->
-  [monoid_assoc_test_case(random:uniform(50) - 1, random:uniform(50) - 1, random:uniform(50) - 1)
-    || _ <- lists:seq(1, 10000)].
+    [monoid_assoc_test_case(random:uniform(50) - 1,
+                            random:uniform(50) - 1,
+                            random:uniform(50) - 1)
+     || _ <- lists:seq(1, 10000)].
 
 monoid_assoc_big_test() ->
-  [monoid_assoc_test_case(random:uniform(100) - 1, random:uniform(100) - 1, random:uniform(100) - 1)
-    || _ <- lists:seq(1, 10000)].
+    [monoid_assoc_test_case(random:uniform(100) - 1,
+                            random:uniform(100) - 1,
+                            random:uniform(100) - 1)
+     || _ <- lists:seq(1, 10000)].
